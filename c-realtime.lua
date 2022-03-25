e77285d48e34b807a5b65e4829c03d3ad5fd481f
Aland Mariwan <dev@aland-mariwan.de>
2022-03-25 19:27:12 +0100

    Update c-realtime.lua

--- c-realtime.lua
+++ c-realtime.lua
@@ -1,6 +1,11 @@
 SetMillisecondsPerGameMinute(60000)
 RegisterNetEvent("am_realtime:event")
 AddEventHandler("am_realtime:event", function(h, m, s)
+	Citizen.CreateThread(function()
+		while true do
 	NetworkOverrideClockTime(h, m, s)
+	Citizen.Wait(0)
+	end
+	end)
 end)
 TriggerServerEvent("am_realtime:event")
