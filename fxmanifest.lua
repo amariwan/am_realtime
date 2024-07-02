9e826340e7018bc1d4a9b6c279a7a6f051fe4406
Aland Mariwan <amariwan9@icloud.com>
2024-07-02 13:58:42 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,9 @@
 fx_version "cerulean"
 game "gta5"
+
 author "Aland Mariwan"
-description "change the clock in game to real life clock"
-version "1.0.0"
+description "Change the clock in game to real life clock"
+version "1.0.1"
 
 client_script "c-realtime.lua"
 server_script "s-realtime.lua"
